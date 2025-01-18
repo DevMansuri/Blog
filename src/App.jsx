2b753a697cef2f564af416bb2c32a77ce8fbccff
vishwas-grid <vishwaskumar@grid.suuchi.com>
2025-01-19 03:55:56 +0530

    Update file with Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-// import { useEffect, useState } from "react";
+
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Authors from "./components/Authors";
 
 function App() {
-  // const [posts, setPosts] = useState([]);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:3001/authors")
-  //     .then((response) => response.json())
-  //     .then((data) => setPosts(data))
-  //     .catch((error) => console.error("Error fetching posts:", error));
-  // }, []);
 
   return (
     <>
