013f35e851fd55fd3276424754014c92c12f96c8
vishwas-grid <vishwaskumar@grid.suuchi.com>
2025-01-19 03:53:54 +0530

    Update file with Navbar and add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import './App.css'
+// import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Authors from "./components/Authors";
 
 function App() {
+  // const [posts, setPosts] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3001/authors")
+  //     .then((response) => response.json())
+  //     .then((data) => setPosts(data))
+  //     .catch((error) => console.error("Error fetching posts:", error));
+  // }, []);
 
   return (
-    <h1>Hello</h1>
-  )
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/authors" element={<Authors />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
